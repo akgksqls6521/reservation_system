3a73a7c511469c8b760a6451c18a5607d2dbb00a
Hanbin Ma <qls6521@naver.com>
2016-05-10 20:07:07 +0900

    #3 Second Event Programming

--- app/src/main/java/kr/hs/emirim/qls6521/yeah/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/qls6521/yeah/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.qls6521.yeah;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
         textResult=(TextView)findViewById(R.id.text_result);
         timePick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);//매개변수에 정수형의 상수필드를 쓰면 화면에 보이지 않느다
+
+        radioDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.VISIBLE);//보이게
+                timePick.setVisibility(View.INVISIBLE);//보이지 않게
+            }
+        });/*객체생성한참조값*/
+        radioTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.INVISIBLE);
+                timePick.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 }
